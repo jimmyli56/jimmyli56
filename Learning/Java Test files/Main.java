90c81052e2f0788668bf890ade72389de3daabdc
Wokipokei <jimmyli564656@gmail.com>
2021-08-31 17:41:39 -0700

    add java test files

---
+++ Learning/Java Test files/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String Tf = " True "; 
+        String Question = "Is today a weekend?";
+        System.out.println(Question+Tf);
+    }
+}
+
