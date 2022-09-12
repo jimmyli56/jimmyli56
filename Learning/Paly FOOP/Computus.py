9257c236bee2829e72646e6bf5f5b5fd15ddd07f
Wokipokei <jimmyli564656@gmail.com>
2022-09-12 09:45:24 -0700

    file updates EoClass

--- Learning/Paly FOOP/Computus.py
+++ Learning/Paly FOOP/Computus.py
@@ -1,6 +1,5 @@
-#Computus
-
-y = 2023
+#Computus by Jimmy Li
+y = 2022
 a = y % 19 
 b = y // 100
 c = y % 100
@@ -14,9 +13,7 @@ m = (a+11*h) // 319
 r = (2*e + 2*j - k - h + m + 32) % 7
 n = (h - m + r + 90) // 25
 p = (h - m + r + n + 19) % 32
-month = (h - m + r + 90) // 25
-day = (h - m + r + n + month + 19) % 32
+month = n
+day = (h - m + r + n + 19) % 32
 print("Easter is on", month, day, y)
 print (a, b, c, d, e, g, h, j, k, m, r, n, p,)
-
-
