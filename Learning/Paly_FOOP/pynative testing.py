bdc4fde5decaf9ea51357879a8c4dc1bf129a8f1
Wokipokei <jimmyli564656@gmail.com>
2022-12-08 10:06:48 -0800

    file updates, removed some old files.

--- Learning/Paly_FOOP/pynative testing.py
+++ Learning/Paly_FOOP/pynative testing.py
@@ -2,19 +2,18 @@
 # s1 = "Ault"
 # s2 = "Kelly"
 
-# # expected result: AuKellylt
 # print(s1 + s2 + s1[2:])
 
 # #Exercise 3 
 # def mixed_string(s1, s2):
 #     # get the first character from both string
-#     first_char = s1[0] + s2[0]
+#     first = s1[0] + s2[0]
 
 #     # get the middle character from both string
-#     middle_char = s1[int(len(s1) / 2):int(len(s1) / 2) + 1] + s2[int(len(s2) / 2):int(len(s2) / 2) + 1]
+#     middle = s1[int(len(s1) / 2):int(len(s1) / 2) + 1] + s2[int(len(s2) / 2):int(len(s2) / 2) + 1]
 
 #     # get last character from both string
-#     last_char = s1[len(s1) - 1] + s2[len(s2) - 1]
+#     last= s1[len(s1) - 1] + s2[len(s2) - 1]
 
 #     # adds all of them together
 #     res = first_char + middle_char + last_char
@@ -24,22 +23,47 @@
 # s2 = "Japan"
 # mix_string(s1, s2)
 
-#Exercise 4
+# #Exercise 4
 
-str1 = "PYnAtivE"
-print('Original String:', str1)
-lower = []
-upper = []
-for char in str1:
-    if char.islower():
-        # adds lowercase characters to lower list
-        lower.append(char)
-    else:
-        # adds uppercase characters to lower list
-        upper.append(char)
+# str1 = "PyNaTive"
+# print('Original String:', str1)
+# lower = []
+# upper = []
+# for char in str1:
+#     if char.islower():
+#         # adds lowercase characters to lower list
+#         lower.append(char)
+#     else:
+#         # adds uppercase characters to lower list
+#         upper.append(char)
 
-# Join both list
-sorted_str = ''.join(lower + upper)
-print('Result:', sorted_str)
+# # Join both lists and print
+# sorted_str = ''.join(lower + upper)
+# print('Result:', sorted_str)
 
+# # Exercise 5
 
+# def find_digits_chars_symbols(input_string):
+#     digits = 0
+#     chars = 0
+#     symbols = 0
+#     for i in range(len(input_string)):
+#         if input_string[i].isdigit():
+#             digits = digits + 1
+#         elif input_string[i].isalpha():
+#             chars = chars + 1
+#         else:
+#             symbols = symbols + 1
+#     print("Digits", digits)
+#     print("Chars", chars)
+#     print("Symbols", symbols)
+# input_string = "P@#yn26at^&i5ve"
+# print("total counts of chars, Digits, and symbols \n")
+# find_digits_chars_symbols(input_string)
+
+# # Exercise 16
+
+# str = 'I am 25 years and 10 months old'
+# print("Original string is", str)
+# result = "".join([item for item in str if item.isdigit()])
+# print(result)
