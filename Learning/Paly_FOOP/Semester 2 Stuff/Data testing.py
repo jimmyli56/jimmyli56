e34333bf99ff21f156ba49295391d5fd3a190d5e
Wokipokei <jimmyli564656@gmail.com>
2023-01-17 11:03:43 -0800

    Data testing.py added Bay area GDP data.

--- Learning/Paly_FOOP/Semester 2 Stuff/Data testing.py
+++ Learning/Paly_FOOP/Semester 2 Stuff/Data testing.py
@@ -1,15 +1,22 @@
-import statistics
 sum = 0
-data = [1,1,2,5,7,8]
+data = [308,301,305,309,322,331,333,349,336,338,364,377,400,422,447,483,512,535,524,577]
 for i in data:
     sum += i
 data.sort()
-mean1 = round(sum/len(data),2)
-mid = len(data)//2
-if (len(data)%2 == 1):
-    mid = data[mid]
-print(mid)
-print(statistics.median(data))
+mean = round(sum/len(data),2)
+median = data[len(data)//2]
+mode = max(set(data), key=data.count)
 
-d=dict()
-print(max(d, key=d.get))
+def varience(data):
+    sum = 0
+    for i in data:
+        sum += (i-mean)**2
+    return round(sum/len(data),2)
+
+standard_deviation = round(varience(data)**0.5,2)
+
+print("mean: ",mean)
+print("median: ",median)
+print("mode: ",mode)
+print("varience: ", varience(data))
+print("standard deviation: ", standard_deviation)
