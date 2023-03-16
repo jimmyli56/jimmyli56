848947c39ef655aeb4fd12a4124ec4264669183e
Wokipokei <jimmyli564656@gmail.com>
2023-03-16 12:26:40 -0700

    file updates 3/16

---
+++ Learning/Paly_FOOP/Semester 2 Stuff/Java/testing/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+Math.log()
+Math.round()
+public class Main {
+    public static void main(String[] args) {
+        //Comment
+        System.out.println("Please enter your dog's age:");
+        d_age = Scanner(System.in).nextDouble();
+        String h_age;
+        h_age (16 * Math.log(d_age) + 31);
+        h_age (Math.round(h_age*100))/100.0;
+        scan.close();
+        System.out.println("Your dog's age in human years is: " + h_age);
+    }
+}
