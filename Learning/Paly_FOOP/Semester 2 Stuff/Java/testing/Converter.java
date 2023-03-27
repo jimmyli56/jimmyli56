d25eec965eb6577a5e4322e8aaed51c41433ffbe
Wokipokei <jimmyli564656@gmail.com>
2023-03-27 09:44:51 -0700

    file updates

---
+++ Learning/Paly_FOOP/Semester 2 Stuff/Java/testing/Converter.java
@@ -0,0 +1,19 @@
+public class Converter{
+  //global variables    
+
+
+  //constructor
+
+
+
+  //celcius converter function - takes temp in F and returns C
+
+
+
+  //farenheit converter function - takes temp in C and returns F
+
+
+
+
+  
+}
