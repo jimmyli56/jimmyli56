221856e63e0e783d997024ad6b8c9f50c4f03e3e
Wokipokei <jimmyli564656@gmail.com>
2024-03-01 13:09:22 -0800

    post restore push

---
+++ Learning/CIS250 Files/Xcode/idk testing/idk/idk/idkApp.swift
@@ -0,0 +1,17 @@
+//
+//  idkApp.swift
+//  idk
+//
+//  Created by Jimmy Li on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct idkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
